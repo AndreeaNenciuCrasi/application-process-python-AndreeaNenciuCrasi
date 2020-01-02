6bdc7e33b05d1d7182754085e5efb9992555fa28
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-01-02 18:09:23 +0200

    Added home.html

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/search-mentor')
@@ -168,6 +173,7 @@ def applicants_full_data():
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
